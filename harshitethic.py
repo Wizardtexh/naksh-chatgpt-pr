86cd53ab67f6a63c69154ba1259c04cb0868ca62
𝐍ᴀᴋs𝐇  𝐕ᴀɪ <166161356+Wizardtexh@users.noreply.github.com>
2024-04-14 13:36:44 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -5,7 +5,7 @@ import json, os, string, sys, threading, logging, time, re, random
 import openai
 
 #OpenAI API key
-aienv = os.getenv('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')
+aienv = os.getenv('OPEN_AI_KEY')
 if aienv == None:
     openai.api_key = "22152362"
 else:
